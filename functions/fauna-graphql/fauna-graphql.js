476da1f81fb61475fef939a6b7ed6a0f471e8302
Hal Newton <hal.newton@gmail.com>
2019-09-28 10:46:50 -0700

    try it

--- functions/fauna-graphql/fauna-graphql.js
+++ functions/fauna-graphql/fauna-graphql.js
@@ -1,7 +1,10 @@
-const { ApolloServer, gql } = require("apollo-server-lambda")
-const { createHttpLink } = require("apollo-link-http")
-const fetch = require("node-fetch")
-const { introspectSchema, makeRemoteExecutableSchema } = require("graphql-tools")
+const { ApolloServer, gql } = require("apollo-server-lambda");
+const { createHttpLink } = require("apollo-link-http");
+const fetch = require("node-fetch");
+const {
+  introspectSchema,
+  makeRemoteExecutableSchema
+} = require("graphql-tools");
 
 exports.handler = function(event, context, cb) {
   /** required for Fauna GraphQL auth */
@@ -9,32 +12,32 @@ exports.handler = function(event, context, cb) {
     const msg = `
     FAUNADB_SERVER_SECRET missing. 
     Did you forget to install the fauna addon or forgot to run inside Netlify Dev?
-    `
-    console.error(msg)
+    `;
+    console.error(msg);
     return {
       statusCode: 500,
       body: JSON.stringify({ msg })
-    }
+    };
   }
   const b64encodedSecret = Buffer.from(
     process.env.FAUNADB_SERVER_SECRET + ":" // weird but they
-  ).toString("base64")
-  const headers = { Authorization: `Basic ${b64encodedSecret}` }
+  ).toString("base64");
+  const headers = { Authorization: `Basic ${b64encodedSecret}` };
 
   /** standard creation of apollo-server executable schema */
   const link = createHttpLink({
-    uri: "https://graphql.faunadb.net/graphql", // modify as you see fit
+    uri: "https://graphql.fauna.com/graphql", // modify as you see fit
     fetch,
     headers
-  })
+  });
   introspectSchema(link).then(schema => {
     const executableSchema = makeRemoteExecutableSchema({
       schema,
       link
-    })
+    });
     const server = new ApolloServer({
       schema: executableSchema
-    })
-    server.createHandler()(event, context, cb)
-  })
-}
+    });
+    server.createHandler()(event, context, cb);
+  });
+};
